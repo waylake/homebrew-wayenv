e8140f6e888947d5ee9552281fb6d03aa1573b00
dy.hwang <hwangdoyeonyu@gmail.com>
2024-07-09 14:21:05 +0900

    init

---
+++ Formula/wayenv.rb
@@ -0,0 +1,17 @@
+class Wayenv < Formula
+  desc "CLI for managing environment variables"
+  homepage "https://github.com/waylake/wayenv"
+  url "https://github.com/waylake/wayenv/releases/download/v1.0.0/wayenv-m1.zip"
+  sha256 "cbcf78e0dca69f5ee39b4ebc1907f47ae5d0a3a198c798b6786b41dacfeef608"
+  license "MIT"
+
+  depends_on "bun"
+
+  def install
+    bin.install "wayenv"
+  end
+
+  test do
+    assert_match "wayenv", shell_output("#{bin}/wayenv --version")
+  end
+end
