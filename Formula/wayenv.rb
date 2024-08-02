9f6134e76981ebaa58d2162695a6ef0bce18ec4a
dy.hwang <hwangdoyeonyu@gmail.com>
2024-08-02 22:07:35 +0900

    update wayenv version

--- Formula/wayenv.rb
+++ Formula/wayenv.rb
@@ -1,8 +1,8 @@
 class Wayenv < Formula
   desc "CLI for managing environment variables"
   homepage "https://github.com/waylake/wayenv"
-  url "https://github.com/waylake/wayenv/releases/download/v1.0.3/wayenv-m1.zip"
-  sha256 "8ce422316169df51aa663db20e8da661390516ff76212ddab73a9617d9245a84"
+  url "https://github.com/waylake/wayenv/releases/download/v1.0.4/wayenv-m1.zip"
+  sha256 "1fba3112a0f3881c0e7365613c2a33664fce3587867644ad4d57d8f32421b98b"
   license "MIT"
 
   def install
